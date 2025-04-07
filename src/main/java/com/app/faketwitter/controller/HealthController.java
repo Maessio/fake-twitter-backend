4f4952edf9eada89059629dcbde7129617a54181
Maessio <maessiosousa@gmail.com>
2025-04-07 19:29:55 -0300

    feat: creating enironments variables

--- src/main/java/com/app/faketwitter/controller/HealthController.java
+++ src/main/java/com/app/faketwitter/controller/HealthController.java
@@ -2,11 +2,14 @@ package com.app.faketwitter.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/health")
 public class HealthController {
-    @GetMapping("/health")
+
+    @GetMapping()
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
     }
